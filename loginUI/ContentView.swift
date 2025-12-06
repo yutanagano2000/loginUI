7b5a397e0ae89db1d4e76aa3d3ad9ef162f11afd
yuta.nagano <yuta.nagano2000@gmail.com>
2025-12-06 19:45:09 +0900

    画面背景をマットブラック・GoogleボタンとFacebookボタンを追加

--- loginUI/ContentView.swift
+++ loginUI/ContentView.swift
@@ -1,48 +1,47 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var username = ""
-    @State private var password = ""
+    @State private var username=""
+    @State private var password=""
     
     var body: some View{
-        ZStack {
-            Color.black
+        ZStack{
+            Color(red:18/255, green: 18/255,blue: 20/255)
                 .ignoresSafeArea()
-            
-            VStack(spacing: 24){
-                Text("Welcome Back!")
+            VStack(spacing: 20){
+                
+                Text("Welcome back!")
                     .font(.title)
                     .fontWeight(.semibold)
                     .foregroundColor(.white)
                 Text("Please sign in to your account.")
                     .font(.subheadline)
                     .foregroundColor(.gray)
+                
+                
                 VStack(spacing: 16){
                     TextField("Username", text:$username)
                         .padding()
                         .background(Color(red:32/255, green: 34/255, blue: 40/255))
-                        .foregroundStyle(Color.white)
+                        .foregroundColor(.white)
                         .cornerRadius(16)
                     SecureField("Password", text:$password)
                         .padding()
-                        .background(Color(red: 32/255, green: 34/255, blue: 40/255))
-                        .foregroundStyle(Color.white)
+                        .background(Color(red:32/255, green: 34/255, blue: 40/255))
+                        .foregroundColor(.white)
                         .cornerRadius(16)
-
                 }
                 HStack {
                     Spacer()
-                    Button("Forgot password?"){
+                    Button("Forgot Password?"){
                         
                     }
                     .font(.footnote)
-                    .foregroundColor(.gray)
                     .frame(maxWidth: .infinity)
                 }
-                Button(action: {
-                    
-                }){
-                    Text("Sign In")
+                Button(action:{})
+                {
+                    Text("Sign in")
                         .fontWeight(.semibold)
                         .frame(maxWidth: .infinity)
                         .padding()
@@ -50,18 +49,28 @@ struct ContentView: View {
                 .background(Color.indigo)
                 .foregroundColor(.white)
                 .cornerRadius(24)
-                Button(action:{}){
+                Button(action:{})
+                {
                     Text("Sign in with Google")
                         .fontWeight(.semibold)
                         .frame(maxWidth: .infinity)
                         .padding()
                 }
                 .background(Color.white)
-                .foregroundStyle(Color.black)
+                .foregroundColor(.black)
                 .cornerRadius(24)
-                
+                Button(action:{})
+                {
+                    Text("Sign in with FaceBook")
+                        .fontWeight(.semibold)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                }
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(24)
             }
-            .padding(.horizontal, 6)
+            .padding(.horizontal, 4)
         }
     }
 }
